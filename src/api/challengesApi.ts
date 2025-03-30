6ea7ad07d37a0a295a4488836dba2217ddb89747
lavanjan <ravilavanjan23@gmail.com>
2025-03-30 14:20:01 +0530

    fix: challenges Api method

--- src/api/challengesApi.ts
+++ src/api/challengesApi.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import { apiClient } from "./apiClient";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
@@ -8,14 +8,14 @@ export const fetchChallenges = async (filters: {
   language?: number;
   difficulty?: string;
 }) => {
-  const response = await axios.get(`${API_BASE_URL}/dev/challenges`, {
+  const response = await apiClient.get(`${API_BASE_URL}/dev/challenges`, {
     params: filters,
   });
   return response.data;
 };
 
 export const fetchChallengeDetails = async (id: string) => {
-  const response = await axios.get(`${API_BASE_URL}/dev/challenges`, {
+  const response = await apiClient.get(`${API_BASE_URL}/dev/challenges`, {
     params: { id },
   });
   return response.data.data[0];
